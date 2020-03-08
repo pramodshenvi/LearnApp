0087241d9ceae348e20c68c1fe3722371ad07115
Pramod Shenvi <pkshenvi@gmail.com>
2020-03-09 00:47:19 +0530

    Removing SessionDeleteDialogComponent from LearnAppSessionModule, to prevent dual import, since it is being imported in CourseModule

--- src/main/webapp/app/entities/session/session.module.ts
+++ src/main/webapp/app/entities/session/session.module.ts
@@ -4,12 +4,10 @@ import { RouterModule } from '@angular/router';
 import { LearnAppSharedModule } from 'app/shared/shared.module';
 import { SessionDetailComponent } from './session-detail.component';
 import { SessionUpdateComponent } from './session-update.component';
-import { SessionDeleteDialogComponent } from './session-delete-dialog.component';
 import { sessionRoute } from './session.route';
 
 @NgModule({
   imports: [LearnAppSharedModule, RouterModule.forChild(sessionRoute)],
-  declarations: [SessionDetailComponent, SessionUpdateComponent, SessionDeleteDialogComponent],
-  entryComponents: [SessionDeleteDialogComponent]
+  declarations: [SessionDetailComponent, SessionUpdateComponent]
 })
 export class LearnAppSessionModule {}
