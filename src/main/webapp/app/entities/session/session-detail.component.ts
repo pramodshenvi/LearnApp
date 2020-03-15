8589b6928688efea1e9e5ae220e71ddfbc0bd405
Pramod Shenvi <pkshenvi@gmail.com>
2020-03-15 23:56:40 +0530

    Session Page

--- src/main/webapp/app/entities/session/session-detail.component.ts
+++ src/main/webapp/app/entities/session/session-detail.component.ts
@@ -4,17 +4,28 @@ import { ActivatedRoute } from '@angular/router';
 import { ISession } from 'app/shared/model/session.model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SessionDeleteDialogComponent } from './session-delete-dialog.component';
+import { MessengerService } from 'app/shared/util/messenger-service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'jhi-session-detail',
-  templateUrl: './session-detail.component.html'
+  templateUrl: './session-detail.component.html',
+  styleUrls: ['./session.component.scss']
 })
 export class SessionDetailComponent implements OnInit {
   session: ISession | null = null;
+  courseDetails: any = {};
 
-  constructor(protected activatedRoute: ActivatedRoute, protected modalService: NgbModal) {}
+  constructor(
+      protected activatedRoute: ActivatedRoute, 
+      protected modalService: NgbModal, 
+      protected messengerService: MessengerService, 
+      protected router: Router) {}
 
   ngOnInit(): void {
+    this.courseDetails = this.messengerService.getCourseDetails();
+    if(!this.courseDetails)
+      this.router.navigate(["/course"])
     this.activatedRoute.data.subscribe(({ session }) => (this.session = session));
   }
 
